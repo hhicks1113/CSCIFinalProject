10293ceeb6514f9f4d802f2c700a11ceb8d8f14a
hhicks1113 <126603177+hhicks1113@users.noreply.github.com>
2024-12-20 13:09:44 -0600

    React Components
    React Components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function NavBar() {
+    return (
+        <nav>
+            <Link to="/">
+                <img src="/home.gif" alt="Home" width="132" height="27" />
+            </Link> &nbsp;
+            <Link to="/services">
+                <img src="/services.gif" alt="Services" width="132" height="27" />
+            </Link> &nbsp;
+            <Link to="/askvet">
+                <img src="/askthevet.gif" alt="Ask the Vet" width="132" height="27" />
+            </Link> &nbsp;
+            <Link to="/contact">
+                <img src="/contact.gif" alt="Contact" width="132" height="27" />
+            </Link>
+            <Link to="/shop">
+                <img src="/shop.gif" alt="Contact" width="132" height="27" />
+            </Link>
+        </nav>
+    );
+}
+export default NavBar; 
